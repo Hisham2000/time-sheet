5dd06bfdee66be8d9af25d3d1dc3c35f220de372
hanwar <hanwar@ntgclarity.com>
2024-03-22 16:24:41 +0200

    make login standalone and lazy loaded

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -5,7 +5,10 @@ import {ActiveSystemGuard} from "./guards/active-system.guard";
 import {IsHrGuard} from "./guards/is-hr.guard";
 
 export const routes: Routes = [
-  {path: "auth", loadChildren: () => import("./auth/auth.module").then( m => m.AuthModule), canActivate: [ActiveLoginGuard]},
+  {path: "auth", children: [
+      {path: "login", loadComponent: () => (import('./auth/login/login.component').then(m=>m.LoginComponent))},
+      {path: "", pathMatch: "full", redirectTo: '/auth/login'}
+    ]},
   {path: "home", loadChildren: () => import("./home/home.module").then( m=> m.HomeModule), canActivate: [ActiveSystemGuard]},
   {path: "attendance", loadChildren: () => import("./attendence/attendence.module").then(m=>m.AttendenceModule), canActivate: [ActiveSystemGuard]},
   {path: "requests", loadChildren: () => import("./requests/requests.module").then(m=>m.RequestsModule), canActivate: [ActiveSystemGuard]},
--- Front/src/app/auth/login/login.component.ts
+++ Front/src/app/auth/login/login.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
-import {AuthService} from "../auth.service";
+import {FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {MessageService} from "primeng/api";
 import {ServiceUrl} from "../../../../Utilities/ServiceUrl";
 import {ServiceCall} from "../../../../Utilities/ServiceCall";
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  imports: [
+    ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule
+  ],
+  standalone: true
 })
 export class LoginComponent {
 
-  constructor(private authService: AuthService,
-              private router: Router,
+  constructor(private router: Router,
               private _serviceUrl: ServiceUrl,
               private _serviceCall: ServiceCall) {
   }
@@ -42,13 +48,6 @@ export class LoginComponent {
   }
 
   submit() {
-    // this.authService.login(this.myForm.get("email")?.value+"", this.myForm.get("password")?.value+"");
-    // if(localStorage.length == 0 ) {
-    //   this.NotFound = true;
-    // }
-    // else{
-    //   this.NotFound = false;
-    // }
     let url = this._serviceUrl.baseUrl + this._serviceUrl.login;
     let body = {
       "email": this.myForm.get("email")?.value+"",
